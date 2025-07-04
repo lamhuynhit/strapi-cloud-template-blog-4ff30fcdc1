340b58bedc3884e013298b9c963856a3267932db
Lam Huynh <lam.huynh@era.com.sg>
2025-07-04 08:34:31 +0700

    Add

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -552,6 +552,7 @@ export interface ApiNewNew extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    description: Schema.Attribute.RichText;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::new.new'> &
       Schema.Attribute.Private;
